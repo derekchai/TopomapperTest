e531d5d215d17125aa8fbac27e354af1a7fe149f
Derek Chai <chderek9@gmail.com>
2024-06-28 20:16:37 +1200

    Delete replaced code

--- Topomapper/Views/Content/MapViewControllerRepresentable.swift
+++ Topomapper/Views/Content/MapViewControllerRepresentable.swift
@@ -1,94 +1,24 @@
 //
-//  MainMap.swift
+//  MapVCR.swift
 //  Topomapper
 //
-//  Created by Derek Chai on 20/06/2024.
+//  Created by Derek Chai on 28/06/2024.
 //
 
 import Foundation
 import SwiftUI
-import MapKit
 
-struct MapViewControllerRepresentable: UIViewControllerRepresentable {
+struct MapVCR: UIViewControllerRepresentable {
+    let appState: AppState
     
-    
-    // MARK: - Exposed Properties
-    
-    var appState: AppState
-    
-    
-    // MARK: - Coordinator
-    
-    class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: MapViewControllerRepresentable
-        
-        var appState: AppState
-        
-        init(parent: MapViewControllerRepresentable, appState: AppState) {
-            self.parent = parent
-            self.appState = appState
-        }
-    }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator(parent: self, appState: appState)
+    func makeUIViewController(context: Context) -> MapViewController {
+        MapViewController(appState: appState)
     }
 
-    
-    // MARK: - UIViewController
-    
-    func makeUIViewController(context: Context) -> UIViewController {
-        let viewController = UIViewController()
-        let mapView = MKMapView()
+    func updateUIViewController(
+        _ uiViewController: MapViewController,
+        context: Context
+    ) {
         
-        mapView.delegate = context.coordinator
-        mapView.frame = viewController.view.frame
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        mapView.showsCompass = true
-        mapView.showsScale = true
-        
-        mapView.overrideUserInterfaceStyle = .light
-        
-        addTopo50MapOverlay(to: mapView)
-        
-        updateRoutePath(in: mapView)
-        
-        updateStartEndAnnotations(in: mapView)
-        updateSelectedPointAnnotation(in: mapView)
-        
-        let tapGestureRecognizer = UITapGestureRecognizer(
-            target: context.coordinator,
-            action: #selector(context.coordinator.ahandleMapTap)
-        )
-        
-        mapView.addGestureRecognizer(tapGestureRecognizer)
-        
-        viewController.view.addSubview(mapView)
-        
-        return viewController
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        let mapView = uiViewController.view.subviews.first as! MKMapView
-        
-        updateRoutePath(in: mapView)
-        
-        updateStartEndAnnotations(in: mapView)
-        updateSelectedPointAnnotation(in: mapView)
-    }
-    
-    
-    // MARK: - Internal Functions
-    
-    /// Adds a `UITapGestureRecognizer` onto `mapView`.
-    private func addMapTapGestureRecognizer(to mapView: MKMapView) {
-        let tapGestureRecognizer = UITapGestureRecognizer(
-            target: self,
-            action: #selector(MapViewControllerRepresentable.Coordinator.ahandleMapTap)
-        )
-        
-        mapView.addGestureRecognizer(tapGestureRecognizer)
     }
 }
-
