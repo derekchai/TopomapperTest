bae06d3a290a03bdaec21eb49e9724fcf1b7aad3
Derek Chai <chderek9@gmail.com>
2024-07-01 22:32:26 +1200

    Add generic extension method on Double to get value as Measurement in given unit
    Also deprecated computed property meters on Double.

--- Topomapper/Extensions/Double+Measurement.swift
+++ Topomapper/Extensions/Double+Measurement.swift
@@ -8,7 +8,12 @@
 import Foundation
 
 extension Double {
+    @available(*, deprecated, message: "Use inUnit(_:) instead.")
     var meters: Measurement<UnitLength> {
         Measurement(value: self, unit: .meters)
+    }
+    
+    func inUnit<U: Dimension>(_ unit: U) -> Measurement<U> {
+        Measurement(value: self, unit: unit)
     }
 }
