9f53962ca9b504381f658707291b0cf36a18f8e1
Derek Chai <chderek9@gmail.com>
2024-07-04 14:39:44 +1200

    Add nearestPoint(to:) extension method on Array<RoutePoint>
    This method uses a binary search to find the `RoutePoint` in this array whose distanceFromStart property is closest to the distanceFromStart given.

---
+++ Topomapper/Extensions/Array<RoutePoint>+NearestPoint.swift
@@ -0,0 +1,43 @@
+//
+//  Array<RoutePoint>+NearestPoint.swift
+//  Topomapper
+//
+//  Created by Derek Chai on 04/07/2024.
+//
+
+import Foundation
+
+extension Array where Element == RoutePoint {
+    /// Uses a binary search to find the `RoutePoint` in this array whose
+    /// `distanceFromStart` property is closest to the `distanceFromStart` given.
+    func nearestPoint(to distanceFromStart: Double) -> RoutePoint? {
+        guard !self.isEmpty else { return nil }
+        
+        var low: Int = 0
+        var high: Int = self.count - 1
+        
+        while low <= high {
+            let mid: Int = (low + high) / 2
+            let midPoint = self[mid]
+            
+            if midPoint.distanceFromStart == distanceFromStart {
+                return midPoint
+            } else if midPoint.distanceFromStart < distanceFromStart {
+                low = mid + 1
+            } else {
+                high = mid + 1
+            }
+        }
+        
+        let lowerBoundPoint = self[Swift.max(low - 1, 0)]
+        let upperBoundPoint = self[Swift.min(low, self.count - 1)]
+        
+        if abs(lowerBoundPoint.distanceFromStart - distanceFromStart) < abs(
+            upperBoundPoint
+                .distanceFromStart - distanceFromStart) {
+            return lowerBoundPoint
+        } else {
+            return upperBoundPoint
+        }
+    }
+}
